3a0d2380d1f5eaf27c0f0ac6e139bcfc8f3385be
Aonullahi <adebayoaonullahi@yahoo.com>
2020-10-13 05:43:00 +0100

    procfile

--- mycustomers/settings.py
+++ mycustomers/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -137,11 +136,3 @@ STATIC_URL = '/static/'
 #
 #}
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
-
-#MEDIA_URL = '/static/'
-
-django_heroku.settings(locals())
