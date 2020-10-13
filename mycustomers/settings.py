3cf87c505408c908a3cb613345d749e9fce11b75
Aonullahi <adebayoaonullahi@yahoo.com>
2020-10-13 06:17:47 +0100

    django-heroku

--- mycustomers/settings.py
+++ mycustomers/settings.py
@@ -138,4 +138,13 @@ STATIC_URL = '/static/'
 #
 #}
 
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
+#MEDIA_URL = '/static/'
+
 django_heroku.settings(locals())
